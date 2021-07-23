d948a4dc8086499838524ade75b3dcd754f238e1
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-23 05:21:53 +0000

    update

---
+++ app-sample/layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      color="primary"
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      clipped-right
+      app
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+        @click="$router.push('/')"
+      >
+        <span class="hidden-sm-and-down">{{ title }}</span>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'GDrive Manager'
+    }
+  }
+}
+</script>
