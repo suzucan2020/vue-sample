423d376bcdf37209e9f60be8c94cabcc57000797
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-23 18:18:28 +0000

    update

--- app-sample/layouts/default.vue
+++ app-sample/layouts/default.vue
@@ -1,17 +1,46 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
-    </v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+<template>
+  <v-toolbar
+    color="purple"
+    dark
+  >
+    <v-toolbar-title>Title</v-toolbar-title>
 
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+    <v-divider
+      class="mx-4"
+      vertical
+    ></v-divider>
+
+    <span class="subheading">My Home</span>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn text>
+        News
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>
+        Blog
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+
+      <v-btn text>
+        Music
+      </v-btn>
+
+      <v-divider vertical></v-divider>
+    </v-toolbar-items>
+
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </v-toolbar>
+</template>
+
 
     <v-main>
       <!--  -->
