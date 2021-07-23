153e59362294f23db0ac003fe58d2e07aa7cb289
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-23 17:43:48 +0000

    update

---
+++ app-sample/pages/sample2.vue
@@ -0,0 +1,28 @@
+<template>
+<v-container>
+  <v-row>
+    <v-col cols="12">
+      <carousel />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="6">
+      <card />
+    </v-col>
+    <v-col cols="6">
+      <card />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="4">
+      <card2 />
+    </v-col>
+    <v-col cols="4">
+      <card2 />
+    </v-col>
+    <v-col cols="4">
+      <card2 />
+    </v-col>
+  </v-row>
+</v-container>
+</template>
