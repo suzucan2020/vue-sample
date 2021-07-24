5e7ce3419316c432ed56404816716fc1ada08b5b
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-24 02:54:19 +0000

    update

---
+++ app-sample/components/card4.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-row dense>
+    <v-col
+      v-for="card in cards"
+      :key="card.title"
+      :cols="card.flex"
+    >
+      <v-card
+        class="mx-auto"
+        max-width="400"
+      >
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+        </v-img>
+
+        <v-card-text class="text--primary">
+          <div> {{ card.title }} </div>
+        </v-card-text>
+
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+        { title: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+      ],
+    }),
+  }
+</script>
--- app-sample/pages/index.vue
+++ app-sample/pages/index.vue
@@ -9,11 +9,16 @@
     </v-col>
   </v-row>
   <v-row>
-    <v-col cols="4">
+    <v-col cols="6">
       <card2 />
     </v-col>
-    <v-col cols="4">
+    <v-col cols="6">
       <card3 />
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col cols="12">
+      <card4 />
     </v-col>
   </v-row>
 </v-container>
