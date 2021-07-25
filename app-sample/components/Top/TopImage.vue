09145fa09fcc0a8b68031e82102d88453abe06ec
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 07:22:42 +0000

    update

--- app-sample/components/Top/TopImage.vue
+++ app-sample/components/Top/TopImage.vue
@@ -1,6 +1,6 @@
 <template>
     <v-img
-      class="white--text align-center"
+      class="white--text align-end"
       height="400px"
       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
     >
