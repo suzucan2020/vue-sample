b422cd16b15279cf98e9b09d0bd881168791b7dd
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 07:46:45 +0000

    update

--- app-sample/pages/index.vue
+++ app-sample/pages/index.vue
@@ -8,11 +8,32 @@
   <v-row>
     <h2>Hello</h2>
   </v-row>
+  
   <v-row>
     <v-col cols="12">
-      <TopNews />
+        <v-container fluid>
+<div v-for
+    v-for="post in posts"
+    :key="post.id"
+  >
+  <v-row dense>
+    <v-col cols="6">
+    <p> {{ post.date }} </p>
+    </v-col>
+
+    <v-col cols="6">
+    <p><a v-bind:href="post.link">{{ post.title.rendered }}</a></p>
+    </v-col>
+  </v-row>
+  </div>
+  </v-container>
+
     </v-col>
   </v-row> 
+
+
+
+
   <v-row>
     <v-col cols="12">
       <TopImageText />
@@ -20,3 +41,30 @@
   </v-row>
 </v-container>
 </template>
+
+<script>
+export default {
+  asyncData({ $axios }) {
+    return $axios.get("http://13.231.178.0/wp-json/wp/v2/posts").then(res => {
+      return {
+        posts: res.data
+      }
+    })
+  },
+  data() {
+    return {
+      message: "hello nuxt ",
+    }
+  },
+  methods: {
+    resetUser: function () {
+      this.posts = []
+    },
+    fetchUser: function () {
+      this.$axios.get("http://13.231.178.0/wp-json/wp/v2/posts").then(res => {
+        this.posts = res.data
+      })
+    },
+  }
+}
+</script>
