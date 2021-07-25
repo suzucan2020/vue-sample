dab270b047e95a864bd609a6df8b80c0b148ad8e
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 00:25:23 +0000

    update

--- app-sample/components/card2.vue
+++ app-sample/components/card2.vue
@@ -8,7 +8,7 @@
       height="200px"
       src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
     >
-      <v-row justify="center" align-content="center">
+      <v-row justify="center">
       < v-card-title >hello world</v-card-title>
       </v-row>
     </v-img>
