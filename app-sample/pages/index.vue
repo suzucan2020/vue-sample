24b21d34d53ce7b1d9be697a5ea24bff738c3c03
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 01:43:00 +0000

    update

--- app-sample/pages/index.vue
+++ app-sample/pages/index.vue
@@ -2,28 +2,12 @@
 <v-container fluid>
   <v-row>
     <v-col cols="12">
-      <ImageText />
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col cols="6">
-      <carousel />
-    </v-col>
-    <v-col cols="6">
-      <card />
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col cols="6">
-      <card2 />
-    </v-col>
-    <v-col cols="6">
-      <card3 />
+      <TopImageText />
     </v-col>
   </v-row>
   <v-row>
     <v-col cols="12">
-      <card4 />
+      <Topcard4 />
     </v-col>
   </v-row>
 </v-container>
