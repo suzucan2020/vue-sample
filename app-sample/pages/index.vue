d6df934983e986dd495c420b57e54468e738a7e5
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 07:58:31 +0000

    update

--- app-sample/pages/index.vue
+++ app-sample/pages/index.vue
@@ -1,14 +1,18 @@
 <template>
 <v-container fluid>
+
   <v-row>
     <v-col cols="12">
       <TopImage />
     </v-col>
   </v-row>
+
   <v-row justify="center" align-content="center">
+    <v-col cols="12">
     <h2>Hello</h2>
+    </v-col>
   </v-row>
-  
+
   <v-row>
     <v-col cols="12">
         <v-container fluid>
@@ -27,18 +31,15 @@
   </v-row>
   </div>
   </v-container>
-
     </v-col>
   </v-row> 
-
-
-
 
   <v-row>
     <v-col cols="12">
       <TopImageText />
     </v-col>
   </v-row>
+
 </v-container>
 </template>
 
