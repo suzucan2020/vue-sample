f2eabe05063652e3517627bccc936a7315308365
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-25 01:35:59 +0000

    update

--- app-sample/components/ImageText.vue
+++ app-sample/components/ImageText.vue
@@ -6,6 +6,8 @@
     >
       <v-row justify="center">
           <v-card-title>hello world</v-card-title>
+      </v-row>
+      <v-row justify="center">
           <p>Enjoy Nuxt !!!</p>
       </v-row>
     </v-img>
