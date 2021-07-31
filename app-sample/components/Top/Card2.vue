303f97438c573aa4b29a0742a01cbbdf2e50560e
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-31 02:57:36 +0000

    update

--- app-sample/components/Top/Card2.vue
+++ app-sample/components/Top/Card2.vue
@@ -35,6 +35,8 @@ export default defineComponent({
       cards: [
         { title: 'A', text: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
         { title: 'B', text: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
+        { title: 'C', text: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
+        { title: 'D', text: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
       ],
     })
 
