b60ab6e61454004dffede128b6302f14c353ef16
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-31 04:23:25 +0000

    update

--- app-sample/components/Top/Card2.vue
+++ app-sample/components/Top/Card2.vue
@@ -13,7 +13,7 @@
         <v-col cols="6">
         <v-img 
           class="white--text align-end"
-          height="200px"
+          height="400px"
           src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
         >
         </v-img>
