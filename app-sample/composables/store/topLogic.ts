7f04b26a150259d21b8f264409bfac4e8b914bfc
suzucan2020 <suzucan2020@users.noreply.github.com>
2021-07-31 03:35:56 +0000

    update

--- app-sample/composables/store/topLogic.ts
+++ app-sample/composables/store/topLogic.ts
@@ -7,7 +7,6 @@ const topLogic = () => {
       { title: 'A', text: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
       { title: 'B', text: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
       { title: 'C', text: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 6 },
-      { title: 'D', text: 'Favorite road trips', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 6 },
     ],
   })
 
